1b3ccf6ccbcc8d2df787ddb14a542209b62193a3
Kent <719951601@qq.com>
2022-12-09 14:37:05 +0800

    test grammar

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { LRLanguage, LanguageSupport } from '@codemirror/language'
-import {styleTags, tags as t} from "@lezer/highlight"
 import {parser} from "./js.grammar"
 import {parser as jsParser} from "@lezer/javascript"
 import {parseMixed} from "@lezer/common";
@@ -8,12 +7,7 @@ export const jsLanguage = LRLanguage.define({
   parser: parser.configure({
     wrap:parseMixed(node=>{
       return node.name === "JsText" ? {parser: jsParser} : null
-    }),
-    props:[
-      styleTags({
-        "{{ }}": t.paren
-      })
-    ]
+    })
   })
 })
 
